4db2427232b5ad80be1768fdbe45dd83fe51dd35
tiritibambix <69852851+tiritibambix@users.noreply.github.com>
2024-12-26 16:23:43 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -9,10 +9,6 @@ from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
-from dotenv import load_dotenv
-
-# Charger les variables d'environnement
-load_dotenv()
 
 # Initialisation de Flask
 app = Flask(__name__)
@@ -26,7 +22,7 @@ if not os.path.exists(UPLOAD_FOLDER):
 
 # Configuration SMTP
 SMTP_SERVER = os.getenv('SMTP_SERVER')
-SMTP_PORT = os.getenv('SMTP_PORT')
+SMTP_PORT = int(os.getenv('SMTP_PORT'))
 SMTP_EMAIL = os.getenv('SMTP_EMAIL')
 SMTP_PASSWORD = os.getenv('SMTP_PASSWORD')
 
