9c630577b068b2ea587d65e41d6561ff763d1de4
tiritibambix <tiritibambix@free.fr>
2025-01-01 14:11:15 +0100

    Upload ?

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-// Détecter l'URL du backend dynamiquement
+// Détecter l'URL du backend dynamiquement, avec le port 5500
 const backendUrl =
   window.location.hostname === 'localhost'
-    ? 'http://localhost:5500'  // Remplace par le port backend de Docker
+    ? 'http://localhost:5500'  // Backend sur port 5500
     : `http://${window.location.hostname}:5500`;  // Pour d'autres machines
 
 console.log('Backend URL configurée :', backendUrl);
