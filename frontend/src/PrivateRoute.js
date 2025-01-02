4b59e088198d76bce51d400bf972227d3d005bf9
tiritibambix <tiritibambix@free.fr>
2025-01-02 18:47:47 +0100

    Update PrivateRoute.js

--- frontend/src/PrivateRoute.js
+++ frontend/src/PrivateRoute.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
   const authToken = localStorage.getItem('authToken');
-  return authToken ? children : <Redirect to="/login" />;
+  return authToken ? children : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
