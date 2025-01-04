cb24de63a2febbf674fa2df68edba4122d4deac0
tiritibambix <tiritibambix@free.fr>
2025-01-04 17:57:53 +0100

    Update routes.py

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -3,8 +3,9 @@ import uuid
 import hashlib
 import smtplib
 from flask import request, jsonify
-from backend.app import app, db
-from backend.app.models import FileUpload
+
+from . import app, db
+from .models import FileUpload
 
 # Charger l'URL dynamique du backend (par exemple, pour envoyer des notifications)
 BACKEND_URL = os.environ.get('BACKEND_URL', 'http://localhost:5000')
