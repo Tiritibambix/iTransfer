a9bb76140567e5d3324dc20a42346c906e1f46ac
tiritibambix <tiritibambix@free.fr>
2025-01-15 12:53:56 +0100

    include env.sh into dockerfile

--- frontend/env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Remplacer la configuration dans le HTML
-sed -i "s|window.BACKEND_URL = '.*'|window.BACKEND_URL = '${BACKEND_URL}'|g" /usr/share/nginx/html/index.html
-
-# Démarrer nginx
-nginx -g 'daemon off;'
